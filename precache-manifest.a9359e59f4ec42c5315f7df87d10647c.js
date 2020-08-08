f213865db6b40db59507c8a54f037bc66ca41e5e
Rocio del Pilar Esteban <hr.chio@gmail.com>
2020-08-08 19:15:13 -0300

    Updates

---
+++ precache-manifest.a9359e59f4ec42c5315f7df87d10647c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98cce2e0103efaf352e423d82a2c7ffe",
+    "url": "/React-rutina-gym/index.html"
+  },
+  {
+    "revision": "aefd107a55916dc385fe",
+    "url": "/React-rutina-gym/static/css/main.ee31f99e.chunk.css"
+  },
+  {
+    "revision": "2d6a91fa5c73bffd55d2",
+    "url": "/React-rutina-gym/static/js/2.6e825bf4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-rutina-gym/static/js/2.6e825bf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aefd107a55916dc385fe",
+    "url": "/React-rutina-gym/static/js/main.70d1def4.chunk.js"
+  },
+  {
+    "revision": "9aa0f7b723ce8306392c",
+    "url": "/React-rutina-gym/static/js/runtime-main.3ecfc99f.js"
+  },
+  {
+    "revision": "2a5dacc357981686a2a89f27cb37f4b0",
+    "url": "/React-rutina-gym/static/media/fondo.2a5dacc3.jpg"
+  }
+]);
